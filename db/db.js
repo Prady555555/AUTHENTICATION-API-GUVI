4ee4868ee71f64b54b10d9c7e714547c2fa9e502
Prady555555 <pradyweb2002@gmail.com>
2024-10-28 14:48:43 +0530

    database atlas change

--- db/db.js
+++ db/db.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
-const database = "mongodb://localhost:27017/auth";
-
+require("dotenv").config();
 async function connect(params) {
   try {
-    await mongoose.connect(database);
+    await mongoose.connect(process.env.MONGO_KEY);
     console.log("Connected to database");
   } catch (error) {
     console.log("Error connecting to database");
